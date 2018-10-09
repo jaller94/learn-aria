6c2f15ad893cb4d298b1e814debe2d426023ca0a
Christian Paul <info@jaller.de>
2018-10-09 01:51:00 -0700

    Start progressbar by adding the intructions to the plugin defaults

--- assets/ik_progressbar.js
+++ assets/ik_progressbar.js
@@ -1,10 +1,11 @@
 ;(function ( $, window, document, undefined ) {
-	
+
 	var pluginName = 'ik_progressbar',
 		defaults = { // values can be overitten by passing configuration options to plugin constructor 
+			'instructions': 'Press spacebar, or Enter to get progress',
 			'max': 100
 		};
-	
+
 	/**
 	 * @constructs Plugin
 	 * @param {Object} element - Current DOM element from selected collection.
